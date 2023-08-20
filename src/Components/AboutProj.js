7ff4139a83599f1f05438daf591ab8fd70a5469e
sahilbishnoi156 <sahilbishnoi156@gmail.com>
2023-08-20 22:20:16 +0530

    Fixing bugs | fix 1.5

--- src/Components/AboutProj.js
+++ src/Components/AboutProj.js
@@ -10,8 +10,7 @@ export default function AboutProj() {
         Sufferer
         <p className="text-xs flex items-center justify-center gap-4 sm:flex-row flex-col">
           <span>
-            Created By{" "}
-            <a href="https://www.instagram.com/s.ahilbishnoi_/" target="_blank">
+            Created By <a href="https://www.instagram.com/s.ahilbishnoi_/" target="_blank">
               Sahil Bishnoi
             </a>
           </span>
@@ -19,14 +18,32 @@ export default function AboutProj() {
           <span>Supported and expanded by many awesome people.</span>
         </p>
       </div>
+      <div className="w-3/4 flex text-white items-center">
+        <div className="w-full flex items-center justify-center">
+        <img src="https://avatars.githubusercontent.com/u/134002041?s=400&u=491c21a491528da76e818ac5e7f97b8c8810f63f&v=4" alt="Not Found" className="h-40 w-40 rounded-full" />
+        </div>
+        <div className="w-full flex sm:flex-col flex-row">
+          <div className="relative">
+            <img src="https://vegibit.com/wp-content/uploads/2014/04/Javascript-JS.png" alt="js" className="h-12 w-12 rounded-full scale-75 absolute animate-bounce -top-12 -left-12" />
+            <img src="https://camo.githubusercontent.com/edc736634dd35b0f4008e2f7db456136b9fc0e1e7a4078bb72c7352b1bdf8a7e/68747470733a2f2f776f726c64766563746f726c6f676f2e636f6d2f6c6f676f732f6373732d332e737667" alt="css" className="h-12 w-12 rounded-full scale-75 absolute animate-bounce left-20 -top-5" />
+            <img src="https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748_640.png" alt="html" className="h-12 w-12 rounded-full scale-75 absolute animate-bounce -bottom-32 -left-20" />
+            <img src="https://www.drupal.org/files/project-images/nextjs-icon-dark-background.png" alt="next" className="h-12 w-12 rounded-full scale-75 absolute animate-bounce -bottom-44 left-24" />
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png" alt="react" className="h-12 w-12 rounded-full scale-75 absolute animate-bounce top-6 left-56" />
+          </div>
+          <p className="text-lg">Hi! I'm </p>
+          <span className="text-2xl text-yellow-400 mb-4">Sahil Bishnoi</span>
+          <p className="text-sm text-slate-400">Creating amazing websites for over <span className="text-white">2 years </span></p>
+          <p className="text-sm text-slate-400">as <span className="text-white">full stack</span> web developer .</p>
+        </div>
+      </div>
       <div className="w-full flex flex-col justify-evenly items-center gap-4 sm:text-justify text-center">
-        <span className="text-2xl text-gray-400 w-full sm:w-3/4">Credits</span>
+        <span className="text-2xl text-gray-400 w-full sm:w-3/4 border-b-2 pb-2 border-slate-600">Credits</span>
         <div className="w-full sm:w-3/4 text-white flex flex-col gap-2">
           <p className="flex flex-col sm:flex-row sm:items-center gap-2 sm:justify-start items-center justify-center">
             <a
               href="https://www.instagram.com/__.shivanshu.___/"
               target="_blank"
-              className="text-yellow-700 text-xl "
+              className="text-blue-500 text-xl "
             >
               Shivanshu
             </a>{" "}
@@ -36,7 +53,7 @@ export default function AboutProj() {
             <a
               href="https://www.instagram.com/s.ahilbishnoi_/"
               target="_blank"
-              className="text-yellow-700 text-xl "
+              className="text-blue-500 text-xl "
             >
               Sahil
             </a>
@@ -47,7 +64,7 @@ export default function AboutProj() {
             <a
               href="https://github.com/sahilbishnoi156/sufferer"
               target="_blank"
-              className="text-yellow-700 text-xl "
+              className="text-blue-500 text-xl "
             >
               Contributors
             </a>
@@ -56,13 +73,14 @@ export default function AboutProj() {
           </p>
         </div>
       </div>
+      {/* Support div */}
       {supportDiv && (
         <div className="fixed z-50 top-0 left-0 h-screen w-screen backdrop-blur-lg flex items-center justify-center transition " onClick={()=>setSupportDiv(false)}>
           <div className="bg-black text-white sm:p-8 p-4 rounded-full sm:text-2xl text-xl border-2">Option currently unavailable</div>
         </div>
       )}
       <div className="w-full flex flex-col justify-evenly items-center gap-4">
-        <span className="text-2xl text-gray-400 w-full sm:w-3/4">Support</span>
+        <span className="text-2xl text-gray-400 w-full sm:w-3/4 border-b-2 pb-2 border-slate-600 text-center sm:text-start">Support</span>
         <div className="w-full sm:w-3/4 text-white text-center sm:text-start">
           Thanks to everyone who has supported this project. It would not be
           possible without you and your continued support.
@@ -77,7 +95,7 @@ export default function AboutProj() {
         </div>
       </div>
       <div className="w-full flex flex-col justify-evenly items-center gap-4">
-        <span className="text-2xl text-gray-400 w-full sm:w-3/4">Contact</span>
+        <span className="text-2xl text-gray-400 w-full sm:w-3/4 border-b-2 pb-2 border-slate-600 sm:text-start text-center">Contact</span>
         <div className="w-full sm:w-3/4 text-white text-center sm:text-start">
           If you encounter a bug, have a feature request or just want to say hi
           - here are the different ways you can contact me directly.
